2fa5ff279910ef53f7764331cc371ed7f1c5b7fb
高语越 <gaoyuyue@outlook.com>
2019-05-12 17:22:12 +0800

    add test should teacher have name and age and classes

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,52 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students = new ArrayList<>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    public void assignLeader(Student leader) {
+        if (!students.contains(leader)) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.leader = leader;
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,28 @@
 package practice10;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Teacher extends Person {
+    private List<Klass> classes = new LinkedList<>();
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+
 }

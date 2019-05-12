6f462dea10e1e760f8cd9d62d8050f10fe0f3e3d
高语越 <gaoyuyue@outlook.com>
2019-05-12 17:08:33 +0800

    add test should class assign a member student as leader

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,45 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void appendMember(Student student) {
+
+    }
 }

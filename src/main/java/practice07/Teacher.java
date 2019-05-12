2d80821ddd4cb5f48de7b927953cc7f9e6eabb46
高语越 <gaoyuyue@outlook.com>
2019-05-12 15:57:46 +0800

    add test should class have a number

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,23 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach "+ (klass == null ? "No Class" : ("Class " + klass)) +".";
+    }
 }

34eab47ad037087f2282a595528f6f1725a56cdd
高语越 <gaoyuyue@outlook.com>
2019-05-12 16:24:08 +0800

    add test should teacher have name and age and class

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,18 +1,18 @@
 package practice07;
 
 public class Teacher extends Person {
-    private Integer klass;
+    private Klass klass;
 
     public Teacher(String name, int age) {
         super(name, age);
     }
 
-    public Teacher(String name, int age, Integer klass) {
+    public Teacher(String name, int age, Klass klass) {
         super(name, age);
         this.klass = klass;
     }
 
-    public Integer getKlass() {
+    public Klass getKlass() {
         return klass;
     }
 

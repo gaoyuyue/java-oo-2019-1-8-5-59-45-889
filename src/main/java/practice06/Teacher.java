1653edd97458eedb508afdfda5bc73d9bfb53643
高语越 <gaoyuyue@outlook.com>
2019-05-12 15:45:54 +0800

    add test should teacher introduce itself with which class it teaches

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -16,5 +16,8 @@ public class Teacher extends Person {
         return klass;
     }
 
-
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach Class "+ klass +".";
+    }
 }
